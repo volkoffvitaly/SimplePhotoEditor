854a1ecb2bafbe7ab80b5404ac864a430d698f61
volkoffvitaly <58873963+volkoffvitaly@users.noreply.github.com>
2020-05-06 21:43:23 +0300

    3 Filters
    Was added:
    - Sepia filter
    - Negative filter
    - Gray filter

--- app/src/main/java/com/example/photoeditor/EditorActivity.kt
+++ app/src/main/java/com/example/photoeditor/EditorActivity.kt
@@ -2,6 +2,9 @@ package com.example.photoeditor
 
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.os.Parcelable
@@ -9,22 +12,23 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import kotlinx.android.synthetic.main.activity_editor.*
 
 
 
 class EditorActivity : AppCompatActivity() {
 
+    var states: MutableList<Bitmap> = ArrayList()
     lateinit var buttons: Array<Button>
     lateinit var fragments: Array<Fragment>
+    lateinit var ivPhoto: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor)
 
 
-        var ivPhoto = findViewById<ImageView>(R.id.ivPhoto)
+        ivPhoto = findViewById(R.id.ivPhoto)
         ivPhoto.setImageURI(intent.getParcelableExtra<Parcelable>("Image") as Uri)
 
         bFilters.isSelected = true
@@ -57,6 +61,15 @@ class EditorActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        bUndo.setOnClickListener {
+
+        }
+
+        bRedo.setOnClickListener {
+
+        }
+
+        
         bFilters.setOnClickListener {
             turnButtons(0)
         }
@@ -97,5 +110,82 @@ class EditorActivity : AppCompatActivity() {
         for (i in buttons.indices) {
             buttons[i].isSelected = i == k
         }
+    }
+
+
+
+
+
+    fun onNegativeFilter () {
+        val bitmapOld = (ivPhoto.drawable as BitmapDrawable).bitmap
+        val bitmapNew = bitmapOld.copy(Bitmap.Config.ARGB_8888, true)
+
+        for (y in 0 until bitmapOld.height) {
+            for (x in 0 until bitmapOld.width) {
+                val oldPixel = bitmapOld.getPixel(x, y)
+
+                val r = 255 - Color.red(oldPixel)
+                val g = 255 - Color.green(oldPixel)
+                val b = 255 - Color.blue(oldPixel)
+
+                bitmapNew.setPixel(x, y, Color.rgb(r, g ,b))
+            }
+        }
+
+        ivPhoto.setImageBitmap(bitmapNew)
+        states.add(bitmapOld)
+    }
+
+
+
+    fun onSepiaFilter() {
+        val bitmapOld = (ivPhoto.drawable as BitmapDrawable).bitmap
+        val bitmapNew = bitmapOld.copy(Bitmap.Config.ARGB_8888, true)
+
+        for (y in 0 until bitmapOld.height) {
+            for (x in 0 until bitmapOld.width) {
+                val oldPixel = bitmapOld.getPixel(x, y)
+
+                val r = Color.red(oldPixel)
+                val g = Color.green(oldPixel)
+                val b = Color.blue(oldPixel)
+
+                var red = (r * 0.393 + g * 0.769 + b * 0.189).toInt()
+                var green = (r * 0.349 + g * 0.686 + b * 0.168).toInt()
+                var blue = (r * 0.272 + g * 0.534 + b * 0.131).toInt()
+
+                red = if (red > 255) 255 else red
+                green = if (green > 255) 255 else green
+                blue = if (blue > 255) 255 else blue
+
+                bitmapNew.setPixel(x, y, Color.rgb(red, green, blue))
+            }
+        }
+
+        ivPhoto.setImageBitmap(bitmapNew)
+        states.add(bitmapOld)
+    }
+
+
+
+    fun onGrayFilter() {
+        val bitmapOld = (ivPhoto.drawable as BitmapDrawable).bitmap
+        val bitmapNew = bitmapOld.copy(Bitmap.Config.ARGB_8888, true)
+
+        for (y in 0 until bitmapOld.height) {
+            for (x in 0 until bitmapOld.width) {
+                val oldPixel = bitmapOld.getPixel(x, y)
+
+                val r = Color.red(oldPixel)
+                val g = Color.green(oldPixel)
+                val b = Color.blue(oldPixel)
+                val grey = (r * 0.2126 + g * 0.7152 + b * 0.0722).toInt()
+
+                bitmapNew.setPixel(x, y, Color.rgb(grey, grey, grey))
+            }
+        }
+
+        ivPhoto.setImageBitmap(bitmapNew)
+        states.add(bitmapOld)
     }
 }

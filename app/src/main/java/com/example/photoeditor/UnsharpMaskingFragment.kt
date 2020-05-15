07ec7880169d1e2b2deb6ce3dd784f1f545a51e7
volkoffvitaly <58873963+volkoffvitaly@users.noreply.github.com>
2020-05-15 15:36:01 +0300

    Fix UnsharpMasking()

--- app/src/main/java/com/example/photoeditor/UnsharpMaskingFragment.kt
+++ app/src/main/java/com/example/photoeditor/UnsharpMaskingFragment.kt
@@ -22,10 +22,11 @@ class UnsharpMaskingFragment : Fragment() {
     private var ivPhoto: Bitmap? = null
     private var blurredPhoto: Bitmap? = null
 
-    //Parameters (Seek'и нужны на эти 3 параметра)
+    //Parameters for Unsharp Masking
     private var radius = 1
     private var amount = 0.1f //float
     private var threshold = 0
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.unsharp_masking_fragment, container, false)
@@ -39,60 +40,62 @@ class UnsharpMaskingFragment : Fragment() {
             //EditorActivity.States.states.add(ivPhoto!!)
         }
 
-        tThreshold.text = "Threshold: 0"
-        tAmount.text = "Amount: 0.1"
+
         tRadius.text = "Radius: 1"
+        tAmount.text = "Amount: 0.1"
+        tThreshold.text = "Threshold: 0"
 
+
+        // RADIUS
         seekRadius.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-
             override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
                 tRadius.text = "Radius: ${seekRadius.progress}"
                 tRadius.isSelected = true
             }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar) {
-
-            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
 
             override fun onStopTrackingTouch(seekBar: SeekBar) {
                 radius = seekRadius.progress
                 tRadius.isSelected = false
             }
         })
+        // RADIUS
 
-        seekThreshold.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
 
-            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
-                tThreshold.text = "Threshold: ${seekThreshold.progress}"
-                tThreshold.isSelected = true
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar) {
-
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar) {
-                threshold = seekThreshold.progress
-                tThreshold.isSelected = false
-            }
-        })
-
+        // AMOUNT
         seekAmount.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-
             override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
                 tAmount.text = "Amount: ${(seekAmount.progress.toFloat() / 10)}"
                 tAmount.isSelected = true
             }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar) {
-
-            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
 
             override fun onStopTrackingTouch(seekBar: SeekBar) {
                 amount = (seekAmount.progress.toFloat() / 10)
                 tAmount.isSelected = false
             }
         })
+        // AMOUNT
+
+
+        // THRESHOLD
+        seekThreshold.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                tThreshold.text = "Threshold: ${seekThreshold.progress}"
+                tThreshold.isSelected = true
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                threshold = seekThreshold.progress
+                tThreshold.isSelected = false
+            }
+        })
+        // THRESHOLD
+
 
         bApply.setOnClickListener(){
             doAsync {
@@ -103,7 +106,7 @@ class UnsharpMaskingFragment : Fragment() {
                     seekRadius.isEnabled = false
                     activity!!.progressLoading!!.visibility = View.VISIBLE
                 }
-                unsharp()
+                unsharpMasking(amount, threshold)
                 uiThread {
                     activity!!.progressLoading!!.visibility = View.INVISIBLE
                 }
@@ -139,21 +142,15 @@ class UnsharpMaskingFragment : Fragment() {
         }
     }
 
-    private fun unsharp(){
-        blurredPhoto = boxBlur(ivPhoto!!, radius)
 
-        val originalPixels =  Array(ivPhoto!!.width, {IntArray(ivPhoto!!.height)})
-        val blurredPixels = Array(blurredPhoto!!.width, {IntArray(blurredPhoto!!.height)})
 
-        for (j in 0 until ivPhoto!!.height) {
-            for (i in 0 until ivPhoto!!.width) {
-                originalPixels[i][j] = ivPhoto!!.getPixel(i, j)
-                blurredPixels[i][j] = blurredPhoto!!.getPixel(i, j)
-            }
+    private fun showConfirmBar() {
+        if (activity!!.confirmBar!!.visibility == View.INVISIBLE){
+            activity!!.confirmBar!!.visibility = View.VISIBLE
         }
-
-        unsharpMask(originalPixels, blurredPixels, amount, threshold)
     }
+
+
 
     private fun boxBlur(bitmap: Bitmap, range: Int): Bitmap? {
         assert(range and 1 == 0) { "Range must be odd." }
@@ -171,150 +168,212 @@ class UnsharpMaskingFragment : Fragment() {
         boxBlurVertical(pixels, width, height, range / 2)
 
         canvas.drawBitmap(pixels, 0, width, 0.0f, 0.0f, width, height, true, null)
+
+
         return blurred
     }
 
 
-    private fun boxBlurHorizontal(pixels: IntArray, w: Int, h: Int, halfRange: Int) {
+    private fun boxBlurHorizontal(pixels: IntArray, width: Int, height: Int, halfRange: Int) {
+        val newColors = IntArray(width)
+
         var index = 0
-        val newColors = IntArray(w)
-        for (y in 0 until h) {
-            var hits = 0
-            var r: Long = 0
-            var g: Long = 0
-            var b: Long = 0
-            for (x in -halfRange until w) {
-                val oldPixel = x - halfRange - 1
+        var hits: Int
+        var red: Long
+        var green: Long
+        var blue: Long
+
+        var oldPixel: Int
+        var oldColor: Int
+
+        var newPixel: Int
+        var newColor: Int
+
+
+        for (y in 0 until height) {
+            hits = 0
+            red = 0
+            green = 0
+            blue = 0
+
+            for (x in -halfRange until width) {
+                oldPixel = x - halfRange - 1
+
                 if (oldPixel >= 0) {
-                    val color = pixels[index + oldPixel]
-                    if (color != 0) {
-                        r -= Color.red(color)
-                        g -= Color.green(color)
-                        b -= Color.blue(color)
+                    oldColor = pixels[index + oldPixel]
+
+                    if (oldColor != 0) {
+                        red -= Color.red(oldColor)
+                        green -= Color.green(oldColor)
+                        blue -= Color.blue(oldColor)
                     }
+
                     hits--
                 }
-                val newPixel = x + halfRange
-                if (newPixel < w) {
-                    val color = pixels[index + newPixel]
-                    if (color != 0) {
-                        r += Color.red(color)
-                        g += Color.green(color)
-                        b += Color.blue(color)
+
+                newPixel = x + halfRange
+
+                if (newPixel < width) {
+                    newColor = pixels[index + newPixel]
+
+                    if (newColor != 0) {
+                        red += Color.red(newColor)
+                        green += Color.green(newColor)
+                        blue += Color.blue(newColor)
                     }
+
                     hits++
                 }
+
                 if (x >= 0) {
-                    newColors[x] = Color.rgb((r / hits).toInt(),
-                        (g / hits).toInt(), (b / hits).toInt()
-                    )
+                    newColors[x] = Color.rgb((red / hits).toInt(), (green / hits).toInt(), (blue / hits).toInt())
                 }
             }
-            for (x in 0 until w) {
+
+            for (x in 0 until width) {
                 pixels[index + x] = newColors[x]
             }
-            index += w
+
+            index += width
         }
     }
 
 
-    private fun boxBlurVertical(pixels: IntArray, w: Int, h: Int, halfRange: Int) {
-        val newColors = IntArray(h)
-        val oldPixelOffset = -(halfRange + 1) * w
-        val newPixelOffset = halfRange * w
-        for (x in 0 until w) {
-            var hits = 0
-            var r: Long = 0
-            var g: Long = 0
-            var b: Long = 0
-            var index = -halfRange * w + x
-            for (y in -halfRange until h) {
-                val oldPixel = y - halfRange - 1
+    private fun boxBlurVertical(pixels: IntArray, width: Int, height: Int, halfRange: Int) {
+        val newColors = IntArray(height)
+        val oldPixelOffset = -(halfRange + 1) * width
+        val newPixelOffset = halfRange * width
+
+        var index: Int
+        var hits: Int
+        var red: Long
+        var green: Long
+        var blue: Long
+
+        var oldPixel: Int
+        var oldColor: Int
+
+        var newPixel: Int
+        var newColor: Int
+
+
+        for (x in 0 until width) {
+            index = -halfRange * width + x
+            hits = 0
+            red = 0
+            green = 0
+            blue = 0
+
+            for (y in -halfRange until height) {
+                oldPixel = y - halfRange - 1
+
                 if (oldPixel >= 0) {
-                    val color = pixels[index + oldPixelOffset]
-                    if (color != 0) {
-                        r -= Color.red(color)
-                        g -= Color.green(color)
-                        b -= Color.blue(color)
+                    oldColor = pixels[index + oldPixelOffset]
+
+                    if (oldColor != 0) {
+                        red -= Color.red(oldColor)
+                        green -= Color.green(oldColor)
+                        blue -= Color.blue(oldColor)
                     }
+
                     hits--
                 }
-                val newPixel = y + halfRange
-                if (newPixel < h) {
-                    val color = pixels[index + newPixelOffset]
-                    if (color != 0) {
-                        r += Color.red(color)
-                        g += Color.green(color)
-                        b += Color.blue(color)
+
+                newPixel = y + halfRange
+
+                if (newPixel < height) {
+                    newColor = pixels[index + newPixelOffset]
+
+                    if (newColor != 0) {
+                        red += Color.red(newColor)
+                        green += Color.green(newColor)
+                        blue += Color.blue(newColor)
                     }
+
                     hits++
                 }
+
                 if (y >= 0) {
-                    newColors[y] = Color.rgb((r / hits).toInt(),
-                        (g / hits).toInt(), (b / hits).toInt()
-                    )
+                    newColors[y] = Color.rgb((red / hits).toInt(), (green / hits).toInt(), (blue / hits).toInt())
                 }
-                index += w
+
+                index += width
             }
-            for (y in 0 until h) {
-                pixels[y * w + x] = newColors[y]
+
+            for (y in 0 until height) {
+                pixels[y * width + x] = newColors[y]
             }
         }
     }
 
 
-    private fun unsharpMask(
-        origPixels: Array<IntArray>,
-        blurredPixels: Array<IntArray>,
-        amount: Float,
-        threshold: Int
-    ) {
+    private fun unsharpMasking(amount: Float, threshold: Int) {
+
+        blurredPhoto = boxBlur(ivPhoto!!, radius)
 
         val newBitmap = Bitmap.createBitmap(ivPhoto!!.width, ivPhoto!!.height, Bitmap.Config.ARGB_8888)
 
-        var orgRed = 0
-        var orgGreen = 0
-        var orgBlue = 0
-        var blurredRed = 0
-        var blurredGreen = 0
-        var blurredBlue = 0
-        var usmPixel = 0
+        var red: Int
+        var green: Int
+        var blue: Int
+        var blurredRed: Int
+        var blurredGreen: Int
+        var blurredBlue: Int
+        var unsharpPixel: Int
         val alpha = -0x1000000
+
+        var originalPixel: Int
+        var blurredPixel: Int
+
         for (j in 0 until newBitmap.height) {
             for (i in 0 until newBitmap.width) {
-                val origPixel = origPixels[i][j]
-                val blurredPixel = blurredPixels[i][j]
 
-                orgRed = origPixel shr 16 and 0xff
-                orgGreen = origPixel shr 8 and 0xff
-                orgBlue = origPixel and 0xff
+                // Get Pixels
+                originalPixel = ivPhoto!!.getPixel(i, j)
+                blurredPixel = blurredPhoto!!.getPixel(i, j)
+
+                // Get original colors
+                red = originalPixel shr 16 and 0xff
+                green = originalPixel shr 8 and 0xff
+                blue = originalPixel and 0xff
+
+                // Get blurred colors
                 blurredRed = blurredPixel shr 16 and 0xff
                 blurredGreen = blurredPixel shr 8 and 0xff
                 blurredBlue = blurredPixel and 0xff
 
-                if (abs(orgRed - blurredRed) >= threshold) {
-                    orgRed = (amount * (orgRed - blurredRed) + orgRed).toInt()
-                    orgRed = if (orgRed > 255) 255 else if (orgRed < 0) 0 else orgRed
+                if (abs(red - blurredRed) >= threshold) {
+                    red = (amount * (red - blurredRed) + red).toInt()
+                    red = when {
+                        red > 255 -> 255
+                        red < 0 -> 0
+                        else -> red
+                    }
                 }
-                if (abs(orgGreen - blurredGreen) >= threshold) {
-                    orgGreen = (amount * (orgGreen - blurredGreen) + orgGreen).toInt()
-                    orgGreen = if (orgGreen > 255) 255 else if (orgGreen < 0) 0 else orgGreen
+
+                if (abs(green - blurredGreen) >= threshold) {
+                    green = (amount * (green - blurredGreen) + green).toInt()
+                    green = when {
+                        green > 255 -> 255
+                        green < 0 -> 0
+                        else -> green
+                    }
                 }
-                if (abs(orgBlue - blurredBlue) >= threshold) {
-                    orgBlue = (amount * (orgBlue - blurredBlue) + orgBlue).toInt()
-                    orgBlue = if (orgBlue > 255) 255 else if (orgBlue < 0) 0 else orgBlue
+
+                if (abs(blue - blurredBlue) >= threshold) {
+                    blue = (amount * (blue - blurredBlue) + blue).toInt()
+                    blue = when {
+                        blue > 255 -> 255
+                        blue < 0 -> 0
+                        else -> blue
+                    }
                 }
-                usmPixel = alpha or (orgRed shl 16) or (orgGreen shl 8) or orgBlue
-                newBitmap.setPixel(i, j, usmPixel)
+
+                unsharpPixel = alpha or (red shl 16) or (green shl 8) or blue
+                newBitmap.setPixel(i, j, unsharpPixel)
             }
         }
 
         activity!!.ivPhoto!!.setImageBitmap(newBitmap)
-    }
-
-    private fun showConfirmBar() {
-        if (activity!!.confirmBar!!.visibility == View.INVISIBLE){
-            activity!!.confirmBar!!.visibility = View.VISIBLE
-        }
     }
 }

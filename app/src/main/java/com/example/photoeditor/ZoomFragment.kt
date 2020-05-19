81b040970cb9cb14d589313f6a7ec07dc39c0aa0
volkoffvitaly <58873963+volkoffvitaly@users.noreply.github.com>
2020-05-20 00:48:32 +0300

    Fix zoom

--- app/src/main/java/com/example/photoeditor/ZoomFragment.kt
+++ app/src/main/java/com/example/photoeditor/ZoomFragment.kt
@@ -1,7 +1,6 @@
 package com.example.photoeditor
 
 import android.graphics.Bitmap
-import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -42,23 +41,23 @@ class ZoomFragment : Fragment() {
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar) {
-                /*fZoom(100 - seekZoom.progress)*/
-                resizeBilinear()
+                val scaledBitmap = resizeBilinear((100 + seekZoom.progress).toDouble() / 100)
+                zoomScaled(scaledBitmap)
             }
         })
     }
 
 
 
-    private fun fZoom(percentage: Int){
-        val bitmapNew = Bitmap.createBitmap((ivPhoto!!.width * percentage / 100), (ivPhoto!!.height * percentage / 100), Bitmap.Config.ARGB_8888)
+    private fun zoomScaled(bitmap: Bitmap) {
+        val bitmapNew = Bitmap.createBitmap((ivPhoto!!.width), (ivPhoto!!.height), Bitmap.Config.ARGB_8888)
 
-        val startX: Int = (ivPhoto!!.width - (ivPhoto!!.width * percentage / 100)) / 2
-        val startY: Int = (ivPhoto!!.height - (ivPhoto!!.height * percentage / 100)) / 2
+        val startX: Int = (bitmap.width - ivPhoto!!.width) / 2
+        val startY: Int = (bitmap.height - ivPhoto!!.height) / 2
 
-        for (y in 0 until bitmapNew.height){
-            for (x in 0 until bitmapNew.width){
-                val oldPixel = ivPhoto!!.getPixel(startX + x, startY + y)
+        for (y in 0 until bitmapNew.height) {
+            for (x in 0 until bitmapNew.width) {
+                val oldPixel = bitmap.getPixel(startX + x, startY + y)
                 bitmapNew.setPixel(x, y, oldPixel)
             }
         }
@@ -66,23 +65,17 @@ class ZoomFragment : Fragment() {
         activity!!.ivPhoto!!.setImageBitmap(bitmapNew)
     }
 
-    fun resizeBilinear(){
+
+    fun resizeBilinear(k: Double): Bitmap {
         val pixels = IntArray(ivPhoto!!.width * ivPhoto!!.height)
         ivPhoto!!.getPixels(pixels, 0, ivPhoto!!.width, 0, 0, ivPhoto!!.width, ivPhoto!!.height)
 
 
-
-        val w2 = ivPhoto!!.width * 2 // Здесь я изменил пропорции
-        val h2 = ivPhoto!!.height * 8
-
-        val scaled = Bitmap.createBitmap(
-            w2, h2,
-            Bitmap.Config.ARGB_8888
-        )
-        val cs = Canvas(scaled)
-
-
+        val w2 = (ivPhoto!!.width * k).toInt()
+        val h2 = (ivPhoto!!.height * k).toInt()
         val temp = IntArray(w2 * h2)
+
+
         var a: Int
         var b: Int
         var c: Int
@@ -111,28 +104,14 @@ class ZoomFragment : Fragment() {
                 c = pixels[index + ivPhoto!!.width]
                 d = pixels[index + ivPhoto!!.width + 1]
 
-                // blue element
-                // Yb = Ab(1-w)(1-h) + Bb(w)(1-h) + Cb(h)(1-w) + Db(wh)
-                blue =
-                    (a and 0xff) * (1 - x_diff) * (1 - y_diff) + (b and 0xff) * x_diff * (1 - y_diff) + (c and 0xff) * y_diff * (1 - x_diff) + (d and 0xff) * (x_diff * y_diff)
+                blue = (a and 0xff) * (1 - x_diff) * (1 - y_diff) + (b and 0xff) * x_diff * (1 - y_diff) + (c and 0xff) * y_diff * (1 - x_diff) + (d and 0xff) * (x_diff * y_diff)
+                green = (a shr 8 and 0xff) * (1 - x_diff) * (1 - y_diff) + (b shr 8 and 0xff) * x_diff * (1 - y_diff) + (c shr 8 and 0xff) * y_diff * (1 - x_diff) + (d shr 8 and 0xff) * (x_diff * y_diff)
+                red = (a shr 16 and 0xff) * (1 - x_diff) * (1 - y_diff) + (b shr 16 and 0xff) * x_diff * (1 - y_diff) + (c shr 16 and 0xff) * y_diff * (1 - x_diff) + (d shr 16 and 0xff) * (x_diff * y_diff)
 
-                // green element
-                // Yg = Ag(1-w)(1-h) + Bg(w)(1-h) + Cg(h)(1-w) + Dg(wh)
-                green =
-                    (a shr 8 and 0xff) * (1 - x_diff) * (1 - y_diff) + (b shr 8 and 0xff) * x_diff * (1 - y_diff) + (c shr 8 and 0xff) * y_diff * (1 - x_diff) + (d shr 8 and 0xff) * (x_diff * y_diff)
-
-                // red element
-                // Yr = Ar(1-w)(1-h) + Br(w)(1-h) + Cr(h)(1-w) + Dr(wh)
-                red =
-                    (a shr 16 and 0xff) * (1 - x_diff) * (1 - y_diff) + (b shr 16 and 0xff) * x_diff * (1 - y_diff) + (c shr 16 and 0xff) * y_diff * (1 - x_diff) + (d shr 16 and 0xff) * (x_diff * y_diff)
-                temp[offset++] = -0x1000000 or  // hardcode alpha
-                        (red.toInt() shl 16 and 0xff0000) or
-                        (green.toInt() shl 8 and 0xff00) or
-                        blue.toInt()
+                temp[offset++] = -0x1000000 or (red.toInt() shl 16 and 0xff0000) or (green.toInt() shl 8 and 0xff00) or blue.toInt()
             }
         }
 
-        cs.drawBitmap(temp, 0, w2, 0.0F, 0.0F, w2, h2, true, null)
-        activity!!.ivPhoto!!.setImageBitmap(scaled)
+        return Bitmap.createBitmap(temp, w2, h2, Bitmap.Config.ARGB_8888)
     }
 }
